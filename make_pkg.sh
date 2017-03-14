a35dc37bb2006e2b50f2c080aa59f72b7de68857
John Sanders <john@johnsanders.me>
2017-03-14 09:57:22 -0500

    using standard find/print, then inserting XML with sed.

--- make_pkg.sh
+++ make_pkg.sh
@@ -2,13 +2,14 @@
 
 version=$(git describe | sed -e "s/v//")
 fn=lastpass-joomla-saml-$version.zip
-statFormat="%t%t<filename>%N</filename>%n"
 
 cd plugins/authentication/lpsaml
-(echo "		<filename plugin=\"lpsaml\">lpsaml.php</filename>" ; \
- echo "		<filename>lpsaml.xml</filename>" ; \
- find . -type f ! -name lpsaml.php ! -name lpsaml.xml\* -print0 | xargs -0 stat -f ${statFormat}) | \
-    sed -e "s/\.\///g" > /tmp/filelist.txt
+(
+  echo "		<filename plugin=\"lpsaml\">lpsaml.php</filename>" ; \
+  echo "		<filename>lpsaml.xml</filename>" ; \
+  find . -type f ! -name lpsaml.php ! -name lpsaml.xml\* -print |
+  sed -E -e "s/\.\/(.*)/		<filename>\1<\/filename>/g"
+) > /tmp/filelist.txt
 
 filelist=$(cat /tmp/filelist.txt)
 cat lpsaml.xml.tmpl | sed -e "/PLUGIN_FILES/{
