d72448e6ab94d831dc6072861aae01099dbf5aca
John Sanders <john@johnsanders.me>
2017-03-14 09:26:36 -0500

    first draft of universal stat command in make_pkg.sh. Works on Mac, EC2 Linux.

--- make_pkg.sh
+++ make_pkg.sh
@@ -2,11 +2,12 @@
 
 version=$(git describe | sed -e "s/v//")
 fn=lastpass-joomla-saml-$version.zip
+statFormat="%t%t<filename>%N</filename>%n"
 
 cd plugins/authentication/lpsaml
 (echo "		<filename plugin=\"lpsaml\">lpsaml.php</filename>" ; \
  echo "		<filename>lpsaml.xml</filename>" ; \
- find . -type f ! -name lpsaml.php ! -name lpsaml.xml\* -printf "\t\t<filename>%h/%f</filename>\n") | \
+ find . -type f ! -name lpsaml.php ! -name lpsaml.xml\* -print0 | xargs -0 stat -f ${statFormat}) | \
     sed -e "s/\.\///g" > /tmp/filelist.txt
 
 filelist=$(cat /tmp/filelist.txt)
